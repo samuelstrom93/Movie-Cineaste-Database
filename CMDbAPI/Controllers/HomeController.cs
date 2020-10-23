a4bc8744d5f9732411e22742d6a2250dda162a83
Samuel Ström <samuelstrom93@gmail.com>
2020-10-23 16:30:07 +0200

    små ändringarr

--- CMDbAPI/Controllers/HomeController.cs
+++ CMDbAPI/Controllers/HomeController.cs
@@ -22,45 +22,15 @@ namespace CMDbAPI.Controllers
 
         public async Task<IActionResult> Index()
         {
-            //TODO: Erik använder en tom GetSummaryViewModel i hans repo
-            //var model = await movieRepository.GetSummaryViewModel();
+            //var model = await movieRepository.GetSummarySingleMovie("tt3659388");
 
-            // Döp denna till GetSummaryViewModel istället?
-            var model = await movieRepository.GetSummarySingleMovie("tt3659388");
+            var toplist = await movieRepository.GetTopList();
 
 
-
-            var movies =await movieRepository.GetAllMovieRatings();
-
-
-            Parameter parameter = new Parameter();
-            {
-                parameter.Count = movies.Count();
-                //parameter.Count = 3; Bestämmer hur många som ska vara i topplistan
-
-                //parameter.SortOrder = "Asc"; //Lägst först
-                parameter.SortOrder = "Desc";//Högst först (defaultvärde)
-
-                //parameter.Type = "popularity"; // Sorterar enbart efter hur många som har betygsatt filmen, struntar i hur stor skillnaden är mellan likes & dislikes
-                parameter.Type = "ratings"; // Sorterar efter hur stor skillnaden är mellan likes & dislikes (defaultvärde)
-            }
-
-
-
-            //var toplist = await movieRepository.GetToplist(parameter);
-            
-            var topplistan = await movieRepository.GetTopList();
-
-
-            return View(topplistan);
+            return View(toplist);
         }
 
         
-
-
-
-
-
 
         //public async Task<IActionResult> Summary()
         //{

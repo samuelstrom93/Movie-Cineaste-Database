f4ce2a3bbf8befc2ebd127d5efd3e68a58e399b3
Patric Lindberg <pali1900@student.miun.se>
2020-10-23 12:44:40 +0200

    Lagt till i HomeController: -Hämtar alla filmer som har en rating -Hämtar Topplistan av filmer

--- CMDbAPI/Controllers/HomeController.cs
+++ CMDbAPI/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using CMDbAPI.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -24,6 +26,23 @@ namespace CMDbAPI.Controllers
 
             // Döp denna till GetSummaryViewModel istället?
             var model = await movieRepository.GetSummary("tt3659388");
+
+            var movies =await movieRepository.GetAllMovieRatings();
+
+
+            Parameter parameter = new Parameter();
+            {
+                parameter.Count = movies.Count();
+                //parameter.Count = 3; Bestämmer hur många som ska vara i topplistan
+
+                //parameter.SortOrder = "Asc"; //Lägst först
+                parameter.SortOrder = "Desc";//Högst först (defaultvärde)
+
+                //parameter.Type = "popularity"; // Sorterar enbart efter hur många som har betygsatt filmen, struntar i hur stor skillnaden är mellan likes & dislikes
+                parameter.Type = "ratings"; // Sorterar efter hur stor skillnaden är mellan likes & dislikes (defaultvärde)
+            }             
+            var toplist = await movieRepository.GetToplist(parameter); 
+
             
             return View(model);
         }
